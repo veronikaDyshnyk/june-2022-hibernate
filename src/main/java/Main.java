967d81bdc8c5a9b23a0158048d04151fc4d7e0e3
veronikaDyshnyk <veronika199942@gmail.com>
2022-12-19 12:59:20 +0000

    init

---
+++ src/main/java/Main.java
@@ -0,0 +1,39 @@
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        StandardServiceRegistry serviceRegistry =
+                new StandardServiceRegistryBuilder()
+                        .configure("hibernate.cfg.xml")
+                        .build();
+
+        Metadata metadata =
+                new MetadataSources(serviceRegistry)
+                        .addAnnotatedClass(null) /*!!!!!!! register class*/
+                        .getMetadataBuilder()
+                        .build();
+        SessionFactory sessionFactory = metadata.getSessionFactoryBuilder().build();
+        Session session = sessionFactory.openSession();
+
+        //work space
+
+        session.beginTransaction();
+
+
+        session.getTransaction().commit();
+
+
+
+
+        /*end don't forget*/
+        session.close();
+        sessionFactory.close();
+    }
+}
